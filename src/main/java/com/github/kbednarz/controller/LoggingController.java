1f739c6bf2647fb497e576b399ab1ab4497f3f7e
kamex737 <kamex737@gmail.com>
2016-07-14 21:05:37 +0200

    login.html updated

--- src/main/java/com/github/kbednarz/controller/LoggingController.java
+++ src/main/java/com/github/kbednarz/controller/LoggingController.java
@@ -30,13 +30,13 @@ public class LoggingController {
     public String createPost(Model model,@ModelAttribute("userEntity") UserEntity userEntity){
         userRepository.findByName(userEntity.getName());
         model.addAttribute("isUserExists",true);
-        return "redirect:/index";
+        return "/login";
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/register")
     public String createNewUser(Model model, @ModelAttribute("userEntity") UserEntity userEntity){
         userRepository.save(new UserEntity(userEntity.getName(),userEntity.getPassword()));
         model.addAttribute("isRegistered",true);
-        return "redirect:/index";
+        return "/login";
     }
 }
